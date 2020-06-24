1047f3979a551c9c51dc9173be6450fb8afba0b1
sergej.berezhnoj <sergej.berezhnoj@rtlabs.ru>
2020-06-24 15:57:25 +0300

    add solution for FrogRiverOne

--- CountingElements/FrogRiverOne/FrogRiverOne.js
+++ CountingElements/FrogRiverOne/FrogRiverOne.js
@@ -1,12 +1,20 @@
 function solution(X, A) {
     // write your code in JavaScript (Node.js 8.9.4)
-    let lastStep = 0
+    const result = new Map()
+    let resultValue = 0
 
-    if(X === 1 && A[0] === 1) return 0
-    for (let i = 1; i <= X; i++) {
-        const index = A.indexOf(i)
-        if (index === -1) return -1
-        if (lastStep < index) lastStep = index
+    if(X === 1 && A[0] === 1) return resultValue
+
+    for (let i = 0; i < A.length; i++) {
+        if(!result.has(A[i])) {
+            result.set(A[i], i)
+        }
     }
-    return lastStep
+
+    if (!result.has(X) || result.size < X) return -1
+
+    for (let value of result) {
+        resultValue = value[1]
+    }
+    return resultValue
 }
